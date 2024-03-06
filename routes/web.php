a6d69f0b79271a39cb43295757a6b61246783800
tahuu17 <tahuuthea17@gmail.com>
2024-03-06 19:52:03 +0700

    slicing login, checkout, succes checkout

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('login', function () {
+    return view('login');
+});
+
+
+Route::get('checkout', function () {
+    return view('checkout');
+});
+
+Route::get('succes-checkout', function () {
+    return view('succes_checkout');
+});
